ff37b5a5307b583090af69a3b537935e0ee6ab8f
Josh Crawford <joshua@sgroup.com.au>
2022-07-12 16:17:52 +1000

    Translations

--- src/translations/en/element-index-defaults.php
+++ src/translations/en/element-index-defaults.php
@@ -1,5 +1,9 @@
 <?php
 
 return [
-
+    'Element Indexes' => 'Element Indexes',
+    'Settings' => 'Settings',
+    'Elements' => 'Elements',
+    'Table Columns' => 'Table Columns',
+    'Choose which table columns should be the default for this element, and in which order.' => 'Choose which table columns should be the default for this element, and in which order.',
 ];
