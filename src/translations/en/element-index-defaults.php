f3f674e91e8cb3efb3716e96573b56c904f2e2bf
Josh Crawford <joshua@sgroup.com.au>
2024-04-19 22:17:07 +1000

    Update English translations.

--- src/translations/en/element-index-defaults.php
+++ src/translations/en/element-index-defaults.php
@@ -1,9 +1,4 @@
 <?php
 
 return [
-    'Element Indexes' => 'Element Indexes',
-    'Settings' => 'Settings',
-    'Elements' => 'Elements',
-    'Table Columns' => 'Table Columns',
-    'Choose which table columns should be the default for this element, and in which order.' => 'Choose which table columns should be the default for this element, and in which order.',
 ];
